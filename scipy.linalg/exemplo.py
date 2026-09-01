8d7a69622297e236d25401e5949d52b49963472a
Mateus Rosado <rosado.mateus@aluno.unifapce.edu.br>
2026-09-01 16:49:17 -0300

    Adiciona exemplo prático de álgebra linear com scipy.linalg
    Demonstra a resolução de um sistema de múltiplas equações lineares utilizando a função linalg.solve.

--- scipy.linalg/exemplo.py
+++ scipy.linalg/exemplo.py
@@ -1,1 +1,20 @@
+import numpy as np
+from scipy import linalg
 
+# Dado as seguntes equações lineares
+# Equação 1: 2x + 3y = 8
+# Equação 2: 3x + y = 5
+
+# Matriz A (os coeficientes das variáveis)
+A = np.array([[2, 3], 
+              [3, 1]])
+
+# Vetor b (os resultados de cada equação)
+b = np.array([8, 5])
+
+# Para resolvermos a equação matricial utilizamos a função "solve" do SciPy encontra os valores exatos de x e y
+variaveis = linalg.solve(A, b)
+
+print(f"Valor de x: {variaveis[0]}\nValor de y: {variaveis[1]}")
+# Saída: Valor de x: 1.0
+#        Valor de y: 2.0
