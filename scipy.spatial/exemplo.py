77b93415211ab37569be493b27fdac39bb577a1f
Rafaele Ferreira <rafaeleferreiraprofissional@gmail.com>
2026-08-31 21:58:56 -0300

    Update exemplo.py spatial

--- scipy.spatial/exemplo.py
+++ scipy.spatial/exemplo.py
@@ -1,1 +1,20 @@
+import numpy as np
+from scipy.sparse import csr_matrix
 
+# Criando uma matriz normal (densa) com muitos zeros
+matriz_densa = np.array([
+    [0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 9, 0]
+])
+
+# Convertendo para matriz esparsa
+matriz_esparsa = csr_matrix(matriz_densa)
+
+print("--- Representação Esparsa ---")
+print(matriz_esparsa)
+
+# Saída esperada:
+#   (1, 1)	7
+#   (3, 3)	9
